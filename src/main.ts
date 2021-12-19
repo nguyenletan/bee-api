8e18613bab0a85a573d5e4af4bc51ef57d6bf6ff
Tan Nguyen Le <nguyenletan@outlook.com>
2021-12-19 13:38:27 +1100

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -44,10 +44,10 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('BEE API')
+    .setDescription('The BEE API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('BEE')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
