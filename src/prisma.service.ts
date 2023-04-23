7dd95d0ec9982441da6c2b421ad2c4b7d66f9a3e
nguyenletan <nguyenletan@gmail.com>
2023-04-23 11:11:49 +1000

    upgrade package

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -12,5 +12,15 @@ export class PrismaService
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+
+    this.$on('beforeExit', async () => {
+
+      await app.close();
+
+    });
+
   }
 }
