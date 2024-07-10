accd92930fcc172cad57e9eb61c9fda9b4bf1f35
nguyenletan <nguyenletan@gmail.com>
2024-07-10 18:23:00 +1000

    fixed updating building feature

--- src/bca/bca.controller.ts
+++ src/bca/bca.controller.ts
@@ -5,30 +5,30 @@ import { BcaService } from './bca.service';
 export class BcaController {
   constructor(private readonly bcaService: BcaService) {}
 
-  @Get('civic_community_cultural_institution')
-  findAllCivicCommunityCulturalInstitution() {
-    return this.bcaService.findAllCivicCommunityCulturalInstitution();
-  }
-
-  @Get('educational_institution')
-  findAllEducationalInstitution() {
-    return this.bcaService.findAllEducationalInstitution();
-  }
-
-  @Get('commercial_building')
-  findAllCommercialBuilding() {
-    return this.bcaService.findAllCommercialBuilding();
-  }
-
-  @Get('healthcare_facility')
-  findAllHealthcareFacility() {
-    return this.bcaService.findAllHealthcareFacility();
-  }
-
-  @Get('sport_recreation_centre')
-  findAllSportRecreationCentre() {
-    return this.bcaService.findAllSportRecreationCentre();
-  }
+  // @Get('civic_community_cultural_institution')
+  // findAllCivicCommunityCulturalInstitution() {
+  //   return this.bcaService.findAllCivicCommunityCulturalInstitution();
+  // }
+  //
+  // @Get('educational_institution')
+  // findAllEducationalInstitution() {
+  //   return this.bcaService.findAllEducationalInstitution();
+  // }
+  //
+  // @Get('commercial_building')
+  // findAllCommercialBuilding() {
+  //   return this.bcaService.findAllCommercialBuilding();
+  // }
+  //
+  // @Get('healthcare_facility')
+  // findAllHealthcareFacility() {
+  //   return this.bcaService.findAllHealthcareFacility();
+  // }
+  //
+  // @Get('sport_recreation_centre')
+  // findAllSportRecreationCentre() {
+  //   return this.bcaService.findAllSportRecreationCentre();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
--- src/pcaf_european-commercial-building-emission-factor/pcaf_european-commercial-building-emission-factor.service.ts
+++ src/pcaf_european-commercial-building-emission-factor/pcaf_european-commercial-building-emission-factor.service.ts
@@ -1,60 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { LONG_CACHE_STRATEGY } from '../shared/constants';
+// import { withAccelerate } from '@prisma/extension-accelerate';
+// import { LONG_CACHE_STRATEGY } from '../shared/constants';
 
 @Injectable()
 export class PCAF_EuropeanCommercialBuildingEmissionFactorService {
   constructor(private prismaService: PrismaService) {}
 
-  findAll() {
-    this.prismaService.pCAF_EuropeanCommercialBuildingEmissionFactor.findMany();
-    return `This action returns all pcafEuropeanCommercialBuildingEmissionFactor`;
-  }
-
-  findAllBySourceName(sourceName: string) {
-    return this.prismaService
-      .$extends(withAccelerate())
-      .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
-        where: {
-          sourceName: sourceName,
-        },
-        cacheStrategy: LONG_CACHE_STRATEGY,
-      });
-  }
-
-  findAllBySourceAndBuildingType(sourceName: string, buildingType: string) {
-    return this.prismaService
-      .$extends(withAccelerate())
-      .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
-        where: {
-          sourceName: sourceName,
-          buildingType: buildingType,
-        },
-        cacheStrategy: LONG_CACHE_STRATEGY,
-      });
-  }
-
-  findAllBySourceNameAndCountry(sourceName: string, country: string) {
-    return this.prismaService
-      .$extends(withAccelerate())
-      .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
-        where: {
-          sourceName: sourceName,
-          country: country,
-        },
-        cacheStrategy: LONG_CACHE_STRATEGY,
-      });
-  }
-
-  findOne(id: number) {
-    return this.prismaService
-      .$extends(withAccelerate())
-      .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
-        where: {
-          id: id,
-        },
-        cacheStrategy: LONG_CACHE_STRATEGY,
-      });
-  }
+  // findAll() {
+  //   this.prismaService.pCAF_EuropeanCommercialBuildingEmissionFactor.findMany();
+  //   return `This action returns all pcafEuropeanCommercialBuildingEmissionFactor`;
+  // }
+  //
+  // findAllBySourceName(sourceName: string) {
+  //   return this.prismaService
+  //     .$extends(withAccelerate())
+  //     .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
+  //       where: {
+  //         sourceName: sourceName,
+  //       },
+  //       cacheStrategy: LONG_CACHE_STRATEGY,
+  //     });
+  // }
+  //
+  // findAllBySourceAndBuildingType(sourceName: string, buildingType: string) {
+  //   return this.prismaService
+  //     .$extends(withAccelerate())
+  //     .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
+  //       where: {
+  //         sourceName: sourceName,
+  //         buildingType: buildingType,
+  //       },
+  //       cacheStrategy: LONG_CACHE_STRATEGY,
+  //     });
+  // }
+  //
+  // findAllBySourceNameAndCountry(sourceName: string, country: string) {
+  //   return this.prismaService
+  //     .$extends(withAccelerate())
+  //     .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
+  //       where: {
+  //         sourceName: sourceName,
+  //         country: country,
+  //       },
+  //       cacheStrategy: LONG_CACHE_STRATEGY,
+  //     });
+  // }
+  //
+  // findOne(id: number) {
+  //   return this.prismaService
+  //     .$extends(withAccelerate())
+  //     .pCAF_EuropeanCommercialBuildingEmissionFactor.findMany({
+  //       where: {
+  //         id: id,
+  //       },
+  //       cacheStrategy: LONG_CACHE_STRATEGY,
+  //     });
+  // }
 }
